250985efb5584e6fb8994164ca99adbe121541f5
Jay <jvidorca@gmail.com>
2022-11-23 22:23:16 +0900

    feat:add style no_painting create

--- static/script/paint_create_api.js
+++ static/script/paint_create_api.js
@@ -1,13 +1,48 @@
 window.onload = () =>{
-    console.log("로딩되었음")
+    const style_no = location.href.split('=')[1]
+    console.log(style_no)
+    loadStyle(style_no)
 }
+
+
+
+//스타일 읽어오기
+async function loadStyle(style_no){
+    StyleLoadView(style_no)
+    
+
+}
+
+
+async function StyleLoadView(style_no){
+    const response = await fetch(`${backendBaseUrl}/paintings/new`, {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json",
+        }
+    })
+    response_json = await response.json()
+    console.log(response_json)
+    console.log(response_json[style_no][1])
+    
+    const style_title = response_json[style_no][1]
+
+    const style_name = document.getElementById("style_name")
+    style_name.innerHTML = `<p style="font-size:30px;font-weight: bold;">0${style_no} ${style_title}</p>`
+
+    
+
+}
+
+
+
+
 
 async function uploadImg(){
     const upload_img = {
         "path": document.getElementById("upload_img").value,
     }
 
-    console.log("1")
     const response = await fetch(`${backendBaseUrl}/paintings/file`, {
         method:'POST',
         headers:{
@@ -15,8 +50,5 @@ async function uploadImg(){
         },
         body: JSON.stringify(upload_img)
     })
-
-    console.log(response)
-    console.log("2")
     console.log(upload_img)
 }
--- static/script/paint_style_api.js
+++ static/script/paint_style_api.js
@@ -1,28 +1,36 @@
+$(StyleSelectView)
+
 //Style select
-async function StyleSelectView(id){
-    const response = await fetch(`${backendBaseUrl}/prints/styleselect/`, {
+async function StyleSelectView(){
+    const response = await fetch(`${backendBaseUrl}/paintings/style`, {
         method: 'GET',
         headers: {
             "Content-type": "application/json",
         }
-    }
-    )
+    })
     response_json = await response.json()
+    console.log(response_json)
 
     $('#style-box').empty()
     response_json.forEach(item => {
         $('#style-box').append(
-            `<!-- Style Thumb Image -->
-            <div class="thumb">
-                <a href="product-details.html"><img src="./model_imgs/02_la_muse(1).png" alt="nft live auction thumbnail"></a>
-            </div>
-            <!-- Style name -->
-            <div class="content">
-                <div class="header d-flex-between pt-4 pb-3">
-                    <h3 class="title"><a href="product-details.html">Style : la_muse01</a></h3>
+            `<div class="explore-style-one">
+                <div class="thumb">
+                    <a href="#" onclick="move_create_page(${item[0]})">
+                        <img src="./model_imgs/${item[1]}.png" alt="nft live auction thumbnail">
+                    </a>
+                </div>
+                <!-- Style name -->
+                <div class="content">
+                    <div class="header d-flex-between pt-4 pb-3">
+                        <h3 class="title"><a href="product-details.html">${item[1]}</a></h3>
+                    </div>
                 </div>
             </div>`
         )
     });
 }
 
+function move_create_page(style_no){ 
+    window.location.href = `/paint_create.html?$id=${style_no}`
+}
