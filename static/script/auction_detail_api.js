33b7bd73804fecb92c734a4046107d12d58a4b3c
jiyeong <2jiyeong0@gmail.com>
2022-11-25 15:59:26 +0900

    Feat : 입찰 기능 추가
    - 1. 입찰 기능 추가

--- static/script/auction_detail_api.js
+++ static/script/auction_detail_api.js
@@ -1,8 +1,7 @@
 
-
 window.onload = async function loadAuction(auctionId){
 
-    const response = await fetch('http://127.0.0.1:8000/auctions/1/3/', { method: 'GET'})
+    const response = await fetch('http://127.0.0.1:8000/auctions/detail/3/', { method: 'GET'})
     response_json = await response.json()
 
     console.log(response_json)
@@ -15,32 +14,31 @@ window.onload = async function loadAuction(auctionId){
     const auction_title = document.getElementById("auction_title")
     const auction_owner = document.getElementById("auction_owner")
     const auction_content = document.getElementById("auction_content")
-    // const auction_end_date = document.getElementById("auction_end_date")
     const auction_like_count = document.getElementById("auction_like_count")
     const auction_now_bid = document.getElementById("auction_now_bid")
-    
-    auction_title.innerText = response_json.painting.title
-    auction_content.innerText = response_json.painting.content
+    const auction_now_bidder = document.getElementById("auction_now_bidder")
+
+
+
+    auction_title.innerText = response_json.painting.title   
     auction_owner.innerText = response_json.painting.owner
-    // auction_end_date.innerText = response_json.end_date
+    auction_content.innerText = response_json.painting.content
     auction_like_count.innerText = response_json.auction_like_count
     auction_now_bid.innerText = response_json.now_bid
+    auction_now_bidder.innerText = response_json.bidder
+
     
     // 상세페이지 이미지
     const painting_image = document.getElementById("painting_image")
     let image_url = response_json.painting.after_image
     painting_image.setAttribute("src", `${backendBaseUrl}${image_url}`)
-    
-    console.log(response_json.end_date)
-    console.log(response_json.end_date.split('T')[0])
 
     // 시간 formating
-    const remaining_time = response_json.end_date.split('T')[0]
+    const remaining_time = response_json.end_date
 
     // 경매 마감 남은 시간
     const remainTime = document.querySelector("#remain-time");
     
-
     function diffDay() {
         const masTime = new Date(remaining_time);
         const todayTime = new Date();
@@ -54,10 +52,39 @@ window.onload = async function loadAuction(auctionId){
         
         remainTime.innerText = `${diffDay}일 ${diffHour}시간 ${diffMin}분 ${diffSec}초`;
     }
+
     diffDay();
     setInterval(diffDay, 1000);
+    }
+}
 
-    
+// 낙찰가 Update
+async function BidUpdate(){
+ 
+    const bidData = {
+        "now_bid": document.getElementById("now_bid").value,
+    }   
+
+    const response = await fetch(`${backendBaseUrl}/auctions/detail/3/`, {
+        method: 'PUT',
+        headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("access")
+            },
+        body: JSON.stringify(bidData)
+    }
+    )
+    console.log(response)
+
+    const response_json = await response.json()
+
+    if (response.status === 200) {
+        alert("입찰이 완료되었습니다.")
+        location.reload()
+
+    }else if (response.status === 400)  {  
+        alert(response_json["error"])
     }
 }
 
