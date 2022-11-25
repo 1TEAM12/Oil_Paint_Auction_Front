bcdbe1b533d1a06f687f223b6c8940fd3984432d
Jay <jvidorca@gmail.com>
2022-11-25 18:38:17 +0900

    feat:add comment counts

--- static/script/auction_detail_api.js
+++ static/script/auction_detail_api.js
@@ -107,35 +107,14 @@ async function loadComment(auction_id) {
         }
     })
     response_json2 = await response2.json()
-
+    var counts = Object.keys(response_json2).length
+    count_comments.innerText = counts
+    
     // user nickname 가져오기
     payload_data = localStorage.getItem("payload")
     payload_data = JSON.parse(payload_data)
     user = payload_data.nickname
-    
-    
 
-    // function displayTime(timeValue) {
-    //     var today = new Date();
-    //     var gap = today.getTime() - timeValue;
-    //     var dateObj = newDate(timeValue);
-    //     var str = "";
-
-    //     //24시간이 지나지 않은 댓글
-    //     if (gap < (1000 * 60 * 60 * 24)) {
-    //         var hh = dateObj.getHour();
-    //         var mi = dateObj.getMinutes();
-    //         var ss = dateObj.getSeconds();
-    //         return [(hh > 9? '': '0') + hh, ':', (mi > 9 ? '' : '0') + mi, (ss > 9 ? '' : '0') + ss].join('');
-    //     } else {
-    //     // 24시간이 지난 댓글
-    //         var yy = dateObj.getFullYear();
-    //         var mm = dateObj.getMonth()+1;
-    //         var dd = dateObj.getDate();
-    //         return [yy, '/', (mm > 9 ? '' : '0') + mm, '/', (dd > 9 ? '' : '0') + dd].join('');
-    //     }
-    //     }
-    
 
     $('#comment_box').empty()
     response_json2.forEach(item => {
@@ -201,9 +180,8 @@ async function createComment(){
     const comment_content = {
         "content": content
     }
-
     const response3 = await fetch(`${backendBaseUrl}/auctions/${auction_id}/comments/`, {
-        method: 'POST',
+        method : 'POST',
         headers: {
             Accept: "application/json",
             "Content-type": "application/json",
